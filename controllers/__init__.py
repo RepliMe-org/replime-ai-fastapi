86ff2d3e07f0b8961241f71c3ca0c5370cd18933
Abdelrahman Mohamed <boodymohammed26@gmail.com>
2026-03-26 23:02:05 +0200

    renaming routes to controllers

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,4 +1,4 @@
-from .base import router as base_router
+from .base_controller import router as base_router
 
 __all__ = [
     "base_router",
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import api_router
+from controllers import api_router
 
 app = FastAPI()
 
